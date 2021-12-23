74a6af043404c67303548fbe4ebaea5b11228c5d
jmrosenberger <jmrosenberger0811@yahoo.com>
2021-12-22 23:56:58 -0600

    added locations select dropdown on Adventure Form / Details

--- src/components/adventures/TestForm.js
+++ src/components/adventures/TestForm.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react"
 import { useHistory, useParams } from 'react-router-dom'
 import { createAdventure, getAdventure, updateAdventure } from './AdventureManager.js'
 import { getHumans } from '../growth/GrowthManager.js'
+import { getLocations } from '../locations/LocationManager.js'
 
 export const TestForm = () => {
     const history = useHistory()
@@ -10,11 +11,19 @@ export const TestForm = () => {
     const { adventureId } = useParams()
     const [humans, setHumans] = useState([])
     const [participants, setParticipants] = useState([])
-    
+    const [locations, setLocations] = useState([])
+    const [location, setLocation] = useState({})
+
 
     useEffect(() => {
         getHumans()
             .then(data => setHumans(data))
+    },
+        [])
+
+    useEffect(() => {
+        getLocations()
+            .then(data => setLocations(data))
     },
         [])
 
@@ -27,14 +36,16 @@ export const TestForm = () => {
                 .then(adventureData => {
                     setCurrentAdventure(adventureData)
                     let array = []
-                    for(const participant of adventureData.participants) {
+                    for (const participant of adventureData.participants) {
                         array.push(participant.id)
                     }
                     setParticipants(array)
-        })} else {
+                })
+        } else {
             setCurrentAdventure({
                 title: "",
                 date: Date(),
+                location: 0,
                 participants: [],
                 description: ""
             })
@@ -52,11 +63,20 @@ export const TestForm = () => {
         setCurrentAdventure(newAdventureState)
     }
 
+    const updateLocation = (event) => {
+        const locationState = { ...location}
+        // const value = parseInt(event.target.value)
+        locationState.id = event.target.value
+        console.log(locationState.id)
+        setLocation(locationState.id)
+    }
+    currentAdventure.location = location
+
     const updateParticipants = (event) => {
-        const participantsArray = [ ...participants ]
+        const participantsArray = [...participants]
         const value = parseInt(event.target.value)
 
-        if (participantsArray.includes(value)){
+        if (participantsArray.includes(value)) {
             const index = participantsArray.indexOf(value)
             participantsArray.splice(index, 1)
         } else {
@@ -65,7 +85,8 @@ export const TestForm = () => {
         setParticipants(participantsArray)
     }
 
-console.log(participants)
+    // console.log(location)
+    // console.log(currentAdventure)
 
     return (
         <form className="adventureForm">
@@ -92,6 +113,23 @@ console.log(participants)
             </fieldset>
             <fieldset>
                 <div className="form-group">
+                    <label htmlFor="location">Location: </label>
+                    <select name="location" placeholder="Select Location" className="form-control"
+                        value={location.id}
+                        defaultValue={0}
+                        onChange={updateLocation}>
+                        <option value="0" disabled>Select Location</option>
+                        {
+                            locations.map(
+                                (location) => {
+                                    return <option name={location.id} value={location.id}>{location.park}, {location.city}</option>
+                                })
+                        }
+                    </select>
+                </div>
+            </fieldset>
+            <fieldset>
+                <div className="form-group">
                     {
                         humans.map(
                             (human) => {
@@ -110,7 +148,7 @@ console.log(participants)
             </fieldset>
             <fieldset>
                 <div className="form-group">
-                    <label htmlFor="description">Description: </label>
+                    <label htmlFor="description">Notes: </label>
                     <input type="text" name="description" required autoFocus className="form-control"
                         value={currentAdventure.description}
                         onChange={changeAdventureState}
@@ -123,6 +161,7 @@ console.log(participants)
                 const adventure = {
                     title: currentAdventure.title,
                     date: currentAdventure.date,
+                    location: currentAdventure.location,
                     participants: participants,
                     description: currentAdventure.description
                 }
